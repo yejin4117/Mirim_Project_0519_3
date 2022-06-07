e2e9cfe6fc5c96c0bb14ff7f32a3c44239d87ff6
이 예진 <s2108@e-mirim.hs.kr>
2022-06-07 20:03:53 +0900

    문제1

--- app/src/main/java/kr/hs/emirim/yejin/mirim_project_0519_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/yejin/mirim_project_0519_3/MainActivity.java
@@ -14,46 +14,62 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.DatePicker;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
 public class MainActivity extends AppCompatActivity {
     Chronometer timer;
     RadioGroup rg;
     TimePicker time;
-    CalendarView date;
-    TextView textresult;
+    DatePicker date;
+    TextView textResult;
     int selectedYear, selectedMonth, selectedDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        timer = findViewById(R.id.timer);
-        rg = findViewById(R.id.rg);
-        time = findViewById(R.id.time);
-        date = findViewById(R.id.date);
-        textresult = findViewById(R.id.text_result);
-        Button btnStart = findViewById(R.id.btn_start);
-        Button btnDone = findViewById(R.id.btn_done);
+        timer=findViewById(R.id.timer);
+        rg=findViewById(R.id.rg);
+        time=findViewById(R.id.time);
+        date=findViewById(R.id.date);
+        textResult=findViewById(R.id.text_result);
+        Button btnStart= findViewById(R.id.btn_start);
+        Button btnDone= findViewById(R.id.btn_done);
         btnStart.setOnClickListener(btnListener);
         btnDone.setOnClickListener(btnListener);
         rg.setOnCheckedChangeListener(rgListener);
-        date.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        date.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
                 selectedYear = year;
-                selectedMonth = month + 1;
-                selectedDay = day;
+                selectedMonth=month+1;
+                selectedDay=day;
             }
         });
         time.setVisibility(View.INVISIBLE);
         date.setVisibility(View.INVISIBLE);
 
+        timer.setBase(SystemClock.elapsedRealtime());
     }
     RadioGroup.OnCheckedChangeListener rgListener = new RadioGroup.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
             time.setVisibility(View.INVISIBLE);
             date.setVisibility(View.INVISIBLE);
-            switch (checkedId){
+            switch(checkedId){
                 case R.id.radio_date:
                     date.setVisibility(View.VISIBLE);
                     break;
@@ -67,20 +83,22 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-
-            switch (view.getId()){
+            switch(view.getId()){
                 case R.id.btn_start:
                     timer.setBase(SystemClock.elapsedRealtime());
                     timer.start();
                     timer.setTextColor(Color.RED);
                     break;
                 case R.id.btn_done:
+
                     timer.stop();
                     timer.setTextColor(Color.BLUE);
-                    textresult.setText(selectedYear + "년" + selectedMonth + "원" + selectedDay + "일");
-                    textresult.append(time.getCurrentHour() + "시" + time.getCurrentMinute() + "분 예약 완료됨");
+                    textResult.setText(selectedYear+"년"+selectedMonth+"월"+selectedDay+"일");
+                    textResult.append(time.getCurrentHour()+"시"+time.getCurrentMinute()+"분 예약완료");
                     break;
+
             }
+
         }
     };
 }
